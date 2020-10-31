ec88a81016e8ef684465fb675c86e2462a71d9b7
emineinan <emineinan@users.noreply.github.com>
2020-10-31 12:42:56 +0300

    printing the alphabet added.

---
+++ Alphabet/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String[] args) {
+   char ch='a';
+   while(ch<='z')
+   {
+       System.out.print(ch+" ");
+       ch++;
+   }
+   
+    System.out.println("\n");    
+        
+   char ch2='A';
+   while(ch2<='Z')
+   {
+       System.out.print(ch2+" ");
+       ch2++;
+   }
+}
+}
