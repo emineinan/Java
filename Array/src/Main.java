ab199632b5fb4d8154f418c83a3254c2c76c8b61
emineinan <emineinan@users.noreply.github.com>
2020-10-31 12:39:17 +0300

    array sample added.

---
+++ Array/src/Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main {
+   public static void main(String[] args) {
+       
+   int[] arr={10,20,30,40,50};
+   System.out.println("*************************");
+   for(int i=0;i<5;i++)
+   {
+      System.out.println(arr[i]);
+   }
+}
+    
+}
