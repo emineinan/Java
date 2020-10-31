db618bb80ba9cb190890bab7013ed268c8f53021
emineinan <emineinan@users.noreply.github.com>
2020-10-31 12:58:20 +0300

    printing powers of two added.

---
+++ PowersOfTwo/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+   int e,result;
+   for(int i=0;i<20;i++)
+   {
+       result=1;
+       e=i;
+       while(e>0)
+       {
+           result*=2;
+           e--;
+       }
+       System.out.println("2 to the "+i+" power is "+result);
+   }  
+}
+}
